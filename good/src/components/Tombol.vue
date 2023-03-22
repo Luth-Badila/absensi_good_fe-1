c88465717c4616ac83ddd6c144a406dd076ed1fb
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-03-22 11:41:08 +0700

    actually nothinghappens

--- good/src/components/Tombol.vue
+++ good/src/components/Tombol.vue
@@ -1,37 +1,31 @@
 <template>
-    <button class="w-full box-border flex gap-4"  @click="onClick">
-        <span>
-          <img
-            src="../assets/img/user1.png"
-            alt=""
-            width="50"
-            height="50"
-            class="rounded-full"
-          />
-        </span>
-        <span class="text-left">
-          <h1 class="font-bold text-xl dark:text-gray-200">
-            {{ label }}
-          </h1>
-          <p class="text-gray-400 dark:text-gray-200">online</p>
-        </span>
-      </button>
-      <br>
-  </template>
-  
-  <script>
+  <div>
+    <button class="w-full box-border flex gap-4" @click="onClick">
+      <span>
+        <img src="../assets/img/user1.png" alt="" width="50" height="50" class="rounded-full" />
+      </span>
+      <span class="text-left">
+        <h1 class="font-bold text-xl dark:text-gray-200">
+          {{ label }}
+        </h1>
+        <p class="text-gray-400 dark:text-gray-200">online</p>
+      </span>
+    </button>
+    <br />
+  </div>
+</template>
+
+<script>
 export default {
-  name: 'MyComponent',
+  name: "MyComponent",
   props: {
     nama: String,
-    label: String
+    label: String,
   },
   methods: {
-    onClick(){
+    onClick() {
       console.log("TES OK");
-    }
-  }
-}
+    },
+  },
+};
 </script>
-
-  
