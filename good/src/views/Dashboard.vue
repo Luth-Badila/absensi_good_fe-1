2ed7bbb189dce6bd3bd2324b0fddbf2cdbdba1eb
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-03-22 22:30:15 +0700

    Havemoved card, chart, product, report in file separate file

--- good/src/views/Dashboard.vue
+++ good/src/views/Dashboard.vue
@@ -17,247 +17,14 @@
       <h1 class="text-2xl text-gray-900 dark:text-gray-200 font-medium">Dashboard</h1>
     </div>
     <!-- grid wrapper card -->
-    <div class="wrapper-card grid lg:grid-cols-4 grid-cols-1 md:grid-cols-2 gap-2 mt-5">
-      <!-- card  -->
-      <!-- <div
-        class="card bg-white dark:bg-gray-800 w-full rounded-md p-5 shadow flex"
-      >
-        <div class="p-2 max-w-sm">
-          <div
-            class="bg-orange-200 rounded-full w-14 h-14 text-lg p-3 text-orange-600 mx-auto"
-          >
-            <span class="">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                role="img"
-                width="30px"
-                height="30px"
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="currentColor"
-                  d="M10 16V8a2 2 0 0 1 2-2h9V5c0-1.1-.9-2-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-1h-9a2 2 0 0 1-2-2zm3-8c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h9V8h-9zm3 5.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5s1.5.67 1.5 1.5s-.67 1.5-1.5 1.5z"
-                />
-              </svg>
-            </span>
-          </div>
-        </div>
-
-        <div class="block p-2 w-full">
-          <p class="font-semibold text-gray-900 dark:text-gray-200 text-xl">
-            Rp.2 300 908
-          </p>
-          <h2 class="font-normal text-gray-400 text-md mt-1">Total Payouts</h2>
-        </div>
-      </div> -->
-      <!-- end card -->
-      <!-- <div class="card bg-white dark:bg-gray-800 w-full rounded-md p-5 shadow flex">
-        <div class="p-2 max-w-sm">
-          <div class="bg-green-200 rounded-full w-14 h-14 text-lg p-3 text-green-600 mx-auto">
-            <span class="">
-              <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="30px" height="30px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                <path
-                  fill="currentColor"
-                  d="M5 22h14a2 2 0 0 0 2-2V9a1 1 0 0 0-1-1h-3v-.777c0-2.609-1.903-4.945-4.5-5.198A5.005 5.005 0 0 0 7 7v1H4a1 1 0 0 0-1 1v11a2 2 0 0 0 2 2zm12-12v2h-2v-2h2zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v1H9V7zm-2 3h2v2H7v-2z"
-                />
-              </svg>
-            </span>
-          </div>
-        </div>
-
-        <div class="block p-2 w-full">
-          <p class="font-semibold text-gray-900 dark:text-gray-200 text-xl">256</p>
-          <h2 class="font-normal text-gray-400 text-md mt-1">Total Sales</h2>
-        </div>
-      </div> -->
-      <!-- end card -->
-      <!-- <div class="card bg-white dark:bg-gray-800 w-full rounded-md p-5 shadow flex">
-        <div class="p-2 max-w-sm">
-          <div class="bg-red-200 rounded-full w-14 h-14 text-lg p-3 text-red-600 mx-auto">
-            <span class="">
-              <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="30px" height="30px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
-                <path fill="none" d="M8.007 24.93A4.996 4.996 0 0 1 13 20h6a4.996 4.996 0 0 1 4.993 4.93a11.94 11.94 0 0 1-15.986 0ZM20.5 12.5A4.5 4.5 0 1 1 16 8a4.5 4.5 0 0 1 4.5 4.5Z" />
-                <path
-                  fill="currentColor"
-                  d="M26.749 24.93A13.99 13.99 0 1 0 2 16a13.899 13.899 0 0 0 3.251 8.93l-.02.017c.07.084.15.156.222.239c.09.103.187.2.28.3c.28.304.568.596.87.87c.092.084.187.162.28.242c.32.276.649.538.99.782c.044.03.084.069.128.1v-.012a13.901 13.901 0 0 0 16 0v.012c.044-.031.083-.07.128-.1c.34-.245.67-.506.99-.782c.093-.08.188-.159.28-.242c.302-.275.59-.566.87-.87c.093-.1.189-.197.28-.3c.071-.083.152-.155.222-.24ZM16 8a4.5 4.5 0 1 1-4.5 4.5A4.5 4.5 0 0 1 16 8ZM8.007 24.93A4.996 4.996 0 0 1 13 20h6a4.996 4.996 0 0 1 4.993 4.93a11.94 11.94 0 0 1-15.986 0Z"
-                />
-              </svg>
-            </span>
-          </div>
-        </div>
-
-        <div class="block p-2 w-full">
-          <p class="font-semibold text-gray-900 dark:text-gray-200 text-xl">3569</p>
-          <h2 class="font-normal text-gray-400 text-md mt-1">Total Customers</h2>
-        </div>
-      </div> -->
-      <!-- end card -->
-      <!-- <div class="card bg-white dark:bg-gray-800 w-full rounded-md p-5 shadow flex">
-        <div class="p-2 max-w-sm">
-          <div class="bg-purple-200 rounded-full w-14 h-14 text-lg p-3 text-purple-600 mx-auto">
-            <span class="">
-              <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="30px" height="30px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16">
-                <g fill="currentColor">
-                  <path d="M15 13v1H1.5l-.5-.5V0h1v13h13Z" />
-                  <path d="M13 3.207L7.854 8.354h-.708L5.5 6.707l-3.646 3.647l-.708-.708l4-4h.708L7.5 7.293l5.146-5.147h.707l2 2l-.707.708L13 3.207Z" />
-                </g>
-              </svg>
-            </span>
-          </div>
-        </div>
-
-        <div class="block p-2 w-full">
-          <p class="font-semibold text-gray-900 dark:text-gray-200 text-xl">7 230</p>
-          <h2 class="font-normal text-gray-400 text-md mt-1">Total Visit</h2>
-        </div>
-      </div> -->
-      <!-- end card -->
-    </div>
+    <card />
     <!-- end wrapper card -->
     <!-- Chart -->
-    <!-- <div class="mt-2 lg:flex block lg:gap-2">
-
-
-    
-      <div class="bg-white dark:bg-gray-800 p-5 w-full rounded-md box-border shadow">
-        <h2 class="font-bold text-lg text-gray-800 dark:text-gray-200">Total Sales</h2>
-        <p class="text-gray-400 font-lexend font-normal">your sales chart per-years</p>
-        <span class="float-right mr-20">
-          <h2 class="text-red-500 -mt-12 flex">
-            <span class="mr-2"> 15.9% </span
-            ><span>
-              <Icon icon="akar-icons:arrow-down" />
-            </span>
-          </h2>
-        </span>
-        <span class="float-right">
-          <h2 class="text-green-500 -mt-12 flex">
-            <span class="mr-2"> 87.9% </span
-            ><span>
-              <Icon icon="akar-icons:arrow-up" />
-            </span>
-          </h2>
-        </span>
-        <br />
-        <apexchart
-          width="100%"
-          height="380"
-          type="area"
-          :options="optionsArea"
-          :series="seriesArea"
-          :sparkline="{
-            enabled: true,
-          }"
-        ></apexchart>
-        <br />
-        <hr />
-        <div class="wrapper-button mt-3">
-          <select name="" id="" class="dark:bg-gray-800 dark:hover:bg-gray-700 border-gray-200 dark:border-gray-300 border max-w-lg px-4 py-3 block rounded-md text-gray-500 dark:text-gray-400">
-            <option value="">Last 7 years</option>
-          </select>
-          <button class="uppercase float-right -mt-7 border-b border-red-600 text-red-600">Sales Report</button>
-        </div>
-      </div>
-      <div class="bg-white dark:bg-gray-800 p-5 lg:w-96 lg:mt-0 mt-4 shadow rounded-md w-full">
-        <h2 class="dark:text-gray-200 text-xl font-bold">Daftar User</h2>
-        <p class="text-gray-400">Yang terakhir Absensi</p>
-        <p class="font-medium font-lg mt-5 dark:text-gray-200">PT A</p>
-        <div class="mt-4 grid grid-cols-1 gap-4">
-          <button-list :buttons="buttonItems"></button-list>
-        </div>
-        <div class="mt-4 grid grid-cols-1 gap-4">
-          <p class="font-medium font-lg mt-5 dark:text-gray-200">PT B</p>
-          <button class="w-full box-border flex gap-4">
-            <span>
-              <img src="../assets/img/user4.png" alt="" width="50" height="50" class="rounded-full" />
-            </span>
-            <span class="text-left">
-              <h1 class="font-bold text-xl dark:text-gray-200">Taylor Neal</h1>
-              <p class="text-gray-400 dark:text-gray-200">2 Hours ago</p>
-            </span>
-          </button>
-          <button class="w-full box-border flex gap-4">
-            <span>
-              <img src="../assets/img/user5.png" alt="" width="50" height="50" class="rounded-full" />
-            </span>
-            <span class="text-left">
-              <h1 class="font-bold text-xl dark:text-gray-200">Bruno Barron</h1>
-              <p class="text-gray-400 dark:text-gray-200">4 Hours ago</p>
-            </span>
-          </button>
-        </div>
-      </div>
-    </div> -->
+    <chart />
     <!-- Product -->
-    <!-- <div class="mt-2 lg:flex block lg:gap-2">
-      <div class="mt-2 bg-white dark:bg-gray-800 p-5 w-full rounded-md box-border shadow">
-        <h2 class="font-bold text-lg text-gray-800 dark:text-gray-200">1,780</h2>
-        <p class="text-gray-400 font-lexend font-normal">New products this week</p>
-        <span class="float-right">
-          <h2 class="text-green-500 -mt-12 flex">
-            <span class="mr-2"> 27.9% </span
-            ><span>
-              <Icon icon="akar-icons:arrow-up" />
-            </span>
-          </h2>
-        </span>
-        <div class="wrapper-chart mt-5">
-          <apexchart width="100%" height="380" type="bar" :options="optionsBar" :series="seriesBar"></apexchart>
-          <br />
-          <hr />
-          <div class="wrapper-button mt-3">
-            <select name="" id="" class="dark:bg-gray-800 dark:hover:bg-gray-700 border-gray-200 dark:border-gray-300 border max-w-lg px-4 py-3 block rounded-md text-gray-500 dark:text-gray-400">
-              <option value="">Last 7 days</option>
-            </select>
-            <button class="uppercase float-right -mt-7 border-b border-red-600 text-red-600">Product Report</button>
-          </div>
-        </div>
-      </div>
-      <div class="mt-2 bg-white dark:bg-gray-800 p-5 w-full rounded-md box-border shadow">
-        <h2 class="font-bold text-lg text-gray-800 dark:text-gray-200">5,355</h2>
-        <p class="text-gray-400 font-lexend font-normal">Visitor this week</p>
-        <span class="float-right">
-          <h2 class="text-green-500 -mt-12 flex">
-            <span class="mr-2"> 47.9% </span
-            ><span>
-              <Icon icon="akar-icons:arrow-up" />
-            </span>
-          </h2>
-        </span>
-        <div class="wrapper-chart mt-5">
-          <apexchart width="100%" height="380" type="area" :options="optionsVisitor" :series="seriesVisitor"></apexchart>
-          <br />
-          <hr />
-          <div class="wrapper-button mt-3">
-            <select name="" id="" class="dark:bg-gray-800 dark:hover:bg-gray-700 border-gray-200 dark:border-gray-300 border max-w-lg px-4 py-3 block rounded-md text-gray-500 dark:text-gray-400">
-              <option value="">Last 7 days</option>
-            </select>
-            <button class="uppercase float-right -mt-7 border-b border-red-600 text-red-600">Vistor Report</button>
-          </div>
-        </div>
-      </div>
-      <div class="mt-2 bg-white dark:bg-gray-800 p-5 w-full rounded-md box-border shadow">
-        <h2 class="font-bold text-lg text-gray-800 dark:text-gray-200">475</h2>
-        <p class="text-gray-400 font-lexend font-normal">User signups this week</p>
-
-        <div class="wrapper-chart mt-5">
-          <apexchart width="100%" height="380" type="pie" :options="optionsDonut" :series="seriesDonut"></apexchart>
-          <div class="p-3"></div>
-          <br />
-          <hr />
-          <div class="wrapper-button mt-3">
-            <select name="" id="" class="dark:bg-gray-800 dark:hover:bg-gray-700 border-gray-200 dark:border-gray-300 border max-w-lg px-4 py-3 block rounded-md text-gray-500 dark:text-gray-400">
-              <option value="">Last 7 years</option>
-            </select>
-
-            <button class="uppercase float-right -mt-7 border-b border-red-600 text-red-600">User Report</button>
-          </div>
-        </div>
-      </div>
-    </div> -->
+    <product />
     <!-- Report -->
+    <report />
     <!-- <div class="mt-2 bg-white dark:bg-gray-800 p-5 w-full rounded-md box-border shadow">
       <h2 class="font-bold text-lg text-gray-800 dark:text-gray-200">Latest Transactions</h2>
       <p class="text-gray-400 font-lexend font-normal">This is a list of latest transactions</p>
@@ -312,6 +79,10 @@
 import Tombol from "@/components/Tombol.vue";
 import { Icon } from "@iconify/vue";
 import ButtonList from "@/components/ButtonList.vue";
+import Card from "@/components/Card.vue";
+import Chart from "@/components/Chart.vue";
+import Product from "@/components/Product.vue";
+import Report from "@/components/Report.vue";
 
 import { useMahasiswaStore } from "@/store/userTestStore";
 
@@ -498,6 +269,10 @@ export default {
     Icon,
     Tombol,
     ButtonList,
+    Card,
+    Chart,
+    Product,
+    Report,
   },
   mounted() {},
 };
