7c2e1bf239b6dd289d472ed32df2545f9061ed89
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-03-25 10:28:29 +0700

    made pinia normal

--- good/src/main.js
+++ good/src/main.js
@@ -6,14 +6,22 @@ import { Icon } from "@iconify/vue";
 import VueApexCharts from "vue3-apexcharts";
 import PerfectScrollbar from "vue3-perfect-scrollbar";
 import "vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css";
-// import "flowbite";
 import "./assets/tailwind.css";
 import "./assets/animate.css";
 import "./assets/sass/css/windzo.css";
 
-//createApp(App).use(createPinia())
+const app = createApp(App);
+
 const pinia = createPinia();
-createApp(App).use(router, Icon).use(VueApexCharts).use(pinia).use(PerfectScrollbar).mount("#app");
+app.use(router, Icon);
+app.use(VueApexCharts);
+app.use(pinia);
+app.use(PerfectScrollbar);
+app.mount("#app");
+
+//createApp(App).use(createPinia())
+// const pinia = createPinia();
+// createApp(App).use(router, Icon).use(VueApexCharts).use(pinia).use(PerfectScrollbar).mount("#app");
 
 // router.beforeEach((to, from, next) => {
 //   document.querySelector(".flex-sidebar").classList.add("hidden");
